d9b228c725e9d676602988b64216118ed89bde39
y1su6666 <nana920916@gmail.com>
2023-12-18 00:16:11 +0800

    更正主頁

--- assets/js/member.js
+++ assets/js/member.js
@@ -1,4 +1,9 @@
-function changeContent(){
-    var contentDiv = document.getElementByld('content')
-    coontentDHTML='<p>AAA</p>';
-}
+$(function(){
+    var $li = $('ul.tab-title li');
+        $($li. eq(0) .addClass('active').find('a').attr('href')).siblings('.tab-inner').hide();
+    
+        $li.click(function(){
+            $($(this).find('a'). attr ('href')).show().siblings ('.tab-inner').hide();
+            $(this).addClass('active'). siblings ('.active').removeClass('active');
+        });
+    });
